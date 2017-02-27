96abca08c152fef7955311d17b4ba4ab0362302d
Nick Ng <nick.doth@gmail.com>
2017-02-27 18:43:16 +0800

    argument handling

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ Calculate annual financial analysis data from Google Finance, and generate CSV-f
   git clone https://github.com/nickdoth/fin-ratios
   cd fin-ratios
   npm install
-  # Usage: node index <query>
+  # Usage: node index [-r | -a] <query>
   # (Generate CSV output)
   node index nasdaq:aapl > ../nasdaq-aapl.csv
+  # (Generate CSV of original finance data)
+  node index --raw nasdaq:aapl > ../nasdaq-aapl-raw.csv
 ```
--- index.js
+++ index.js
@@ -1,28 +1,52 @@
-const createCalculation = require('./calc');
 const jsdom = require('jsdom');
+const program = require('commander');
+const createCalculation = require('./calc');
 const { json2csv, tableToObject, concatByColumn } = require('./table-utils');
 
-jsdom.env({
-    url: 'https://www.google.com/finance?fstype=ii&q=' + process.argv[2],
-    userAgent: 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20120101 Firefox/33.0',
-    done: function(err, window) {
-        var document = window.document;
+program
+    .arguments('<query>')
+    .option('-r, --raw', 'Output raw data only, instead of the result')
+    .option('-a, --all', 'Output both the result and the raw data')
+    .parse(process.argv);
 
-        let balanceAnn = tableToObject($('#balannualdiv table'));
-        let incomeAnn = tableToObject($('#incannualdiv table'));
+if (!program.args[0]) {
+    console.error('Error: No query specified.');
+    program.help();
+}
+else {
+    fetchAndCalc();
+}
 
-        // concat two parts
-        let ann = concatByColumn(balanceAnn, incomeAnn);
+function fetchAndCalc() {
+    jsdom.env({
+        url: 'https://www.google.com/finance?fstype=ii&q=' + program.args[0],
+        userAgent: 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20120101 Firefox/33.0',
+        done: function(err, window) {
+            var document = window.document;
 
-        let results = {};
+            let balanceAnn = tableToObject($('#balannualdiv table'));
+            let incomeAnn = tableToObject($('#incannualdiv table'));
 
-        Object.keys(ann).forEach(year => {
-            results[year] = createCalculation(ann[year]);
-        });
+            // concat two parts
+            let ann = concatByColumn(balanceAnn, incomeAnn);
 
-        // process.stdout.write(json2csv(ann));
-        process.stdout.write(json2csv(results));
+            let results = {};
 
-        function $(s) { return document.querySelector(s); }
-    }
-});
+            Object.keys(ann).forEach(year => {
+                results[year] = createCalculation(ann[year]);
+            });
+
+            if (program.raw) {
+                process.stdout.write(json2csv(ann));
+            }
+            else if (program.all) {
+                process.stdout.write(json2csv(concatByColumn(ann, results)));
+            }
+            else {
+                process.stdout.write(json2csv(results));
+            }
+
+            function $(s) { return document.querySelector(s); }
+        }
+    });
+}
